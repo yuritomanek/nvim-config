b4e8da4da71ad9586b946ec5a44e50deffda9892
Yuri Tománek <yuri@tomanek.co.uk>
2024-08-25 22:56:42 +0100

    Formating changes

--- lua/yuritomanek/core/options.lua
+++ lua/yuritomanek/core/options.lua
@@ -13,7 +13,8 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
-opt.wrap = false -- disable line wrapping
+opt.wrap = true
+opt.wrapmargin = 0
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
